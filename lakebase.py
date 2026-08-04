71b7ccda2cb657b15a31f42f0ae5a80a3b57c792
bhavikamange1993@gmail.com <bhavikamange1993@gmail.com>
2026-08-04 19:21:13 +0000

    created some more setup files

---
+++ lakebase.py
@@ -0,0 +1,85 @@
+"""
+Lakebase connection helper.
+"""
+
+import base64
+import os
+
+import psycopg2
+
+from contextlib import contextmanager
+from databricks.sdk import WorkspaceClient
+from psycopg2.extras import RealDictCursor
+
+
+_workspace = WorkspaceClient()
+
+
+SCOPE = os.getenv(
+    "LAKEBASE_SECRET_SCOPE",
+    "database"
+)
+
+KEY = os.getenv(
+    "LAKEBASE_SECRET_KEY",
+    "lakebase-url"
+)
+
+
+def get_lakebase_url():
+
+    secret = _workspace.secrets.get_secret(
+        scope=SCOPE,
+        key=KEY
+    )
+
+    return base64.b64decode(
+        secret.value
+    ).decode("utf-8")
+
+
+@contextmanager
+def get_connection():
+
+    conn = psycopg2.connect(
+        get_lakebase_url(),
+        cursor_factory=RealDictCursor
+    )
+
+    try:
+        yield conn
+
+    finally:
+        conn.close()
+
+
+
+def run_query(sql, params=None):
+
+    with get_connection() as conn:
+
+        with conn.cursor() as cursor:
+
+            cursor.execute(
+                sql,
+                params
+            )
+
+            return cursor.fetchall()
+
+
+
+def run_write(sql, params=None):
+
+    with get_connection() as conn:
+
+        with conn.cursor() as cursor:
+
+            cursor.execute(
+                sql,
+                params
+            )
+
+            conn.commit()
+
+            return cursor.rowcount
